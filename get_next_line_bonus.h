2fa9f98666fc2f7ebe570e4638590ae38f7f0e48
Mckane666 <Pedro>
2021-02-04 18:51:59 +0000

    with bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhenriqu <jhenriqu@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/20 14:19:36 by jhenriqu          #+#    #+#             */
+/*   Updated: 2021/02/04 18:47:00 by jhenriqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
+typedef struct	s_get_next_line
+{
+	int			r;
+	char		*buf;
+	char		*temp;
+	int			check;
+}				t_gnl;
+
+int				get_next_line_bonus(int fd, char **line);
+char			*ft_strjoin(char *s1, char *s2);
+char			*ft_substr(char *s, unsigned int start, size_t len);
+char			*ft_strdup(char *src);
+char			*ft_strchr(const char *s, int n);
+int				ft_strlen(const char *str);
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhenriqu <jhenriqu@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/08 12:26:08 by jhenriqu          #+#    #+#             */
+/*   Updated: 2021/02/04 18:47:16 by jhenriqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int		ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		t;
+	char	*joined;
+
+	if (!s1 || !s2)
+		return (0);
+	if (!(joined = (char *)malloc(sizeof(char)
+	* (ft_strlen(s1) + ft_strlen(s2)) + 1)))
+		return (NULL);
+	*joined = 0;
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	t = 0;
+	while (s2[t] != '\0')
+	{
+		joined[i + t] = s2[t];
+		t++;
+	}
+	joined[i + t] = '\0';
+	free(s1);
+	return (joined);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		j;
+	char	*copy;
+
+	i = 0;
+	while (src[i] != 0)
+	{
+		i++;
+	}
+	copy = (char*)malloc(sizeof(char) * (i + 1));
+	if (!copy)
+	{
+		return (0);
+	}
+	j = 0;
+	while (src[j] != 0)
+	{
+		copy[j] = src[j];
+		j++;
+	}
+	copy[j] = '\0';
+	return (copy);
+}
+
+char	*ft_strchr(const char *s, int n)
+{
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		if (*s == n)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	if (*s == 0 && n == 0)
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char	*sbs;
+	int		i;
+	int		j;
+
+	if (!s)
+		return (NULL);
+	if ((int)start >= ft_strlen(s))
+	{
+		return (ft_strdup(""));
+	}
+	if (!(sbs = malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	j = 0;
+	i = start;
+	while ((size_t)j < len)
+	{
+		sbs[j] = ((char *)s)[i];
+		i++;
+		j++;
+	}
+	sbs[j] = '\0';
+	return (sbs);
+}
